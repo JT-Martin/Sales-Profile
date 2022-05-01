dfe3e701dd8b98026b1a9b6900a339492dce6194
Doooka <o3ean15@gmail.com>
2022-05-01 21:43:22 +1000

    Updated responsiveness for mobile

--- script.js
+++ script.js
@@ -35,8 +35,10 @@ for (i = 0; i < acc.length; i++) {
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
+      panel.style.padding = '0 18px';
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+      panel.style.padding = '20px 18px';
+    }
   });
 }
